21d018ad265e718fc4405f0501bfe84912618acb
Loïc Mahieu <mahieuloic@gmail.com>
2018-11-14 14:59:40 +0100

    create global stylesheet

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { createGlobalStyle } from "styled-components"
 
 import Layout from '../components/layout'
 import Image from '../components/image'
 
+const GlobalStyle =  createGlobalStyle`
+  body {
+    background: red;
+  }
+`
+
 const IndexPage = () => (
   <Layout>
+    <GlobalStyle />
+
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
